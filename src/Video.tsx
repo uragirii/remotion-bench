1d59d76809a6f927361b1b902e31266bb5c5df45
Apoorv Kansal <apoorvkansalak@gmail.com>
2022-10-01 01:14:53 +0530

    feat: added compositions for each test

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,20 +1,56 @@
 import {Composition} from 'remotion';
+import CanvasTest from './Video/CanvasTest';
+import CSSTest from './Video/CSSTest';
 import Remotion from './Video/index';
+import {Introduction} from './Video/Introduction';
+import VideoTest from './Video/VideoTest';
 
-// Each <Composition> is an entry in the sidebar!
+const fps = 30;
+const width = 1920;
+const height = 1080;
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<Composition
-				// You can take the "id" to render a video:
-				// npx remotion render src/index.tsx <id> out/video.mp4
 				id="Main"
 				component={Remotion}
-				durationInFrames={25 * 30}
-				fps={30}
-				width={1920}
-				height={1080}
+				durationInFrames={25 * fps}
+				fps={fps}
+				width={width}
+				height={height}
+			/>
+			<Composition
+				id="Introduction"
+				component={Introduction}
+				durationInFrames={4 * fps}
+				fps={fps}
+				width={width}
+				height={height}
+			/>
+			<Composition
+				id="Video"
+				component={VideoTest}
+				durationInFrames={10 * fps}
+				fps={fps}
+				width={width}
+				height={height}
+			/>
+			<Composition
+				id="Canvas"
+				component={CanvasTest}
+				durationInFrames={5 * fps}
+				fps={fps}
+				width={width}
+				height={height}
+			/>
+			<Composition
+				id="CSS"
+				component={CSSTest}
+				durationInFrames={6 * fps}
+				fps={fps}
+				width={width}
+				height={height}
 			/>
 		</>
 	);
